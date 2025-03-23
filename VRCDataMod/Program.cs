a41aafabda6a364bb7bb21e7a970510223b36c55
Magma <Magma@magma-mc.net>
2025-03-23 16:19:36 +0000

    Updated VRCDataMod To Correctly Support .NET 9

--- VRCDataMod/Program.cs
+++ VRCDataMod/Program.cs
@@ -1,119 +1,81 @@
 ﻿using System;
 using System.IO;
 using System.IO.Compression;
-using Spectre.Console;
-using System.Threading;
 using System.Linq;
 using System.Text.Json;
-namespace VRCDataMod
+using System.Text.Json.Serialization;
+using System.Threading;
+using Spectre.Console;
+namespace VRCDataMod;
+internal class Program
 {
-    internal class Program
+    public static string packPath = "";
+    public static DataPack Pack;
+    static void Main(string[] args)
     {
-        public static string packPath = "";
-        public static DataPack Pack;
-        static void Main(string[] args)
+        try
         {
+            packPath = args[0];
+        }
+        catch { }
+        while (packPath == "")
+        {
+            var path = AnsiConsole.Ask<string>("[red]Enter DataPack Path[/]:").Replace("\"", "");
             try
             {
-                packPath = args[0];
-            }
-            catch { }
-            while (packPath == "")
-            {
-                var path = AnsiConsole.Ask<string>("[red]Enter DataPack Path[/]:").Replace("\"", "");
-                try
+                if (!path.ToUpper().EndsWith(".DP"))
                 {
-                    if (!path.ToUpper().EndsWith(".DP"))
-                    {
-                        AnsiConsole.MarkupLine("[red]Expected[/] '[OrangeRed1].DP[/]'");
-                        throw new Exception();
-                    }
-                    else if (DataPack.ValidPack(File.ReadAllBytes(path)))
-                        packPath = path;
+                    AnsiConsole.MarkupLine("[red]Expected[/] '[OrangeRed1].DP[/]'");
+                    throw new Exception();
                 }
-                catch
-                {
-                    AnsiConsole.MarkupLine("[RED]Invalid DataPack[/]!");
-                    Thread.Sleep(2000);
-                    Console.Clear();
-                }
+                else if (DataPack.ValidPack(File.ReadAllBytes(path)))
+                    packPath = path;
             }
-            AnsiConsole.MarkupLine("[Lime]Valid DataPack Extracting[/]!");
-            Thread.Sleep(1000);
-            Pack = new DataPack(File.ReadAllBytes(packPath));
-            DataPackage dataPackage = Pack.GetDataPackage();
-            while (true)
+            catch
             {
+                AnsiConsole.MarkupLine("[RED]Invalid DataPack[/]!");
+                Thread.Sleep(2000);
                 Console.Clear();
-                AnsiConsole.MarkupLine("[cyan]======================================[/]");
-                AnsiConsole.MarkupLine($"[cyan]World[/]: {dataPackage.WorldName}");
-                AnsiConsole.MarkupLine($"[cyan]WorldHash[/]: {dataPackage.WorldHash}");
-                AnsiConsole.MarkupLine($"[cyan]Version[/]: {dataPackage.Version}");
-                AnsiConsole.MarkupLine($"[cyan]Author[/]: {dataPackage.Author}");
-                if (dataPackage.Discord != "")
-                    AnsiConsole.MarkupLine($"[cyan]Discord[/]: {dataPackage.Discord}");
-                if (dataPackage.Email != "")
-                    AnsiConsole.MarkupLine($"[cyan]Email[/]: {dataPackage.Email}");
-                AnsiConsole.MarkupLine("[cyan]======================================[/]");
-                var Selection = AnsiConsole.Prompt(
-                    new SelectionPrompt<string>()
-                        .AddChoices(new[] {
-                            "Install",
-                            "Reset",
-                            "Exit"
-                        }));
-                if (Selection == "Exit")
-                    return;
+            }
+        }
+        AnsiConsole.MarkupLine("[Lime]Valid DataPack Extracting[/]!");
+        Thread.Sleep(1000);
+        Pack = new DataPack(File.ReadAllBytes(packPath));
+        DataPackage dataPackage = Pack.GetDataPackage();
+        while (true)
+        {
+            Console.Clear();
+            AnsiConsole.MarkupLine("[cyan]======================================[/]");
+            AnsiConsole.MarkupLine($"[cyan]World[/]: {dataPackage.WorldName}");
+            AnsiConsole.MarkupLine($"[cyan]WorldHash[/]: {dataPackage.WorldHash}");
+            AnsiConsole.MarkupLine($"[cyan]Version[/]: {dataPackage.Version}");
+            AnsiConsole.MarkupLine($"[cyan]Author[/]: {dataPackage.Author}");
+            if (dataPackage.Discord != "")
+                AnsiConsole.MarkupLine($"[cyan]Discord[/]: {dataPackage.Discord}");
+            if (dataPackage.Email != "")
+                AnsiConsole.MarkupLine($"[cyan]Email[/]: {dataPackage.Email}");
+            AnsiConsole.MarkupLine("[cyan]======================================[/]");
+            var Selection = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .AddChoices(new[] {
+                        "Install",
+                        "Reset",
+                        "Exit"
+                    }));
+            if (Selection == "Exit")
+                return;
 
-                string contentpath = "";
-                try
-                {
-                    contentpath = GetVRChatContentCachePath();
-                }
-                catch
-                {
-                    contentpath = Path.Combine(GetVRChatPath(), "Cache-WindowsPlayer\\");
-                }
-                if (Selection == "Reset")
-                {
-                    if (!Directory.Exists(Path.Combine(contentpath, dataPackage.WorldHash)))
-                    {
-                        AnsiConsole.MarkupLine($"[RED]Please Visit [/]`{dataPackage.WorldName}` [RED]Before Installing This Pack[/]!");
-                        Thread.Sleep(2000);
-                    }
-                    else
-                    {
-                        DirectoryInfo dir = new DirectoryInfo(Path.Combine(contentpath, dataPackage.WorldHash));
-                        var subDirectories = dir.GetDirectories();
-
-                        if (subDirectories.Length == 0)
-                        {
-                            AnsiConsole.MarkupLine($"[RED]Please Visit [/]`{dataPackage.WorldName}` [RED]Before Installing This Pack[/]!");
-                            Thread.Sleep(2000);
-                        }
-                        else
-                        {
-                            DirectoryInfo lastEditedDirectory = subDirectories
-                                .OrderByDescending(d => d.LastWriteTime)
-                                .FirstOrDefault();
-
-                            if (lastEditedDirectory == null)
-                            {
-                                AnsiConsole.MarkupLine($"[RED]Please Visit [/]`{dataPackage.WorldName}` [RED]Before Installing This Pack[/]!");
-                                Thread.Sleep(2000);
-                            }
-                            else
-                            {
-                                File.Delete(Path.Combine(lastEditedDirectory.FullName, "__data"));
-
-                                AnsiConsole.MarkupLine($"[Lime]Reset World Data[/]!");
-                                Thread.Sleep(2000);
-                            }
-                        }
-                        Thread.Sleep(2000);
-                        continue;
-                    }
-                }
+            var contentpath = "";
+            try
+            {
+                contentpath = GetVRChatContentCachePath();
+            }
+            catch
+            {
+                contentpath = Path.Combine(GetVRChatPath(), "Cache-WindowsPlayer\\");
+            }
+            if (Selection == "Reset")
+            {
                 if (!Directory.Exists(Path.Combine(contentpath, dataPackage.WorldHash)))
                 {
                     AnsiConsole.MarkupLine($"[RED]Please Visit [/]`{dataPackage.WorldName}` [RED]Before Installing This Pack[/]!");
@@ -142,154 +104,184 @@ namespace VRCDataMod
                         }
                         else
                         {
+                            File.Delete(Path.Combine(lastEditedDirectory.FullName, "__data"));
 
-                            byte[] data = Pack.GetDataBytes();
-                            string targetFilePath = Path.Combine(lastEditedDirectory.FullName, "__data");
-                            File.WriteAllBytes(targetFilePath, data);
-
-                            File.WriteAllText(Path.Combine(lastEditedDirectory.FullName, "WorldData.csv"),
-                                $"name,version\n{dataPackage.WorldName.Replace(" ", "").ToLower()},{dataPackage.Version.Trim()}");
-
-                            AnsiConsole.MarkupLine($"[GREEN]Pack installed successfully![/]");
+                            AnsiConsole.MarkupLine($"[Lime]Reset World Data[/]!");
                             Thread.Sleep(2000);
                         }
                     }
+                    Thread.Sleep(2000);
+                    continue;
                 }
             }
-        }
-
-        public static string GetVRChatPath() =>
-            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),"..\\LocalLow\\VRChat\\VRChat\\");
-        public static VRCConfig GetVRChatConfig()
-        {
-            string ConfigData = File.ReadAllText(GetVRChatPath() + "config.json");
-            return JsonSerializer.Deserialize<VRCConfig>(ConfigData);
-        }
-        public static string GetVRChatContentCachePath()
-        {
-            VRCConfig Conf = GetVRChatConfig();
-            if (string.IsNullOrWhiteSpace(Conf.cache_directory))
-                throw new ArgumentNullException();
-            return Path.Combine(Conf.cache_directory, "Cache-WindowsPlayer");
-        }
-    }
-    public class VRCConfig
-    {
-        public int cache_expire_delay
-        {
-            get; set;
-        } = 0;
-        public int cache_size
-        {
-            get; set;
-        } = 0;
-        public string cache_directory
-        {
-            get; set;
-        } = "";
-        public int fpv_steadycam_fov
-        {
-            get; set;
-        } = 0;
-        public int camera_res_height
-        {
-            get; set;
-        } = 0;
-        public int camera_res_width
-        {
-            get; set;
-        } = 0;
-    }
-
-    public class DataPack
-    {
-        private byte[] _packData;
-
-        public DataPack(byte[] packData)
-        {
-            _packData = packData;
-        }
-
-        public static bool ValidPack(byte[] Pack)
-        {
-            try
+            if (!Directory.Exists(Path.Combine(contentpath, dataPackage.WorldHash)))
             {
-                using (var memoryStream = new MemoryStream(Pack))
-                using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Read))
-                {
-                    bool hasPackageJson = false;
-                    bool hasData = false;
-
-                    foreach (var entry in archive.Entries)
-                    {
-                        if (entry.FullName == "package.json")
-                            hasPackageJson = true;
-                        if (entry.FullName == "__data")
-                            hasData = true;
-                    }
-
-                    return hasPackageJson && hasData;
-                }
+                AnsiConsole.MarkupLine($"[RED]Please Visit [/]`{dataPackage.WorldName}` [RED]Before Installing This Pack[/]!");
+                Thread.Sleep(2000);
             }
-            catch
+            else
             {
-                return false;
-            }
-        }
+                DirectoryInfo dir = new DirectoryInfo(Path.Combine(contentpath, dataPackage.WorldHash));
+                var subDirectories = dir.GetDirectories();
 
-        public byte[] GetDataBytes()
-        {
-            using (var memoryStream = new MemoryStream(_packData))
-            using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Read))
-            {
-                var dataEntry = archive.GetEntry("__data");
-                if (dataEntry != null)
+                if (subDirectories.Length == 0)
                 {
-                    using (var dataStream = dataEntry.Open())
-                    using (var dataMemoryStream = new MemoryStream())
-                    {
-                        dataStream.CopyTo(dataMemoryStream);
-                        return dataMemoryStream.ToArray();
-                    }
+                    AnsiConsole.MarkupLine($"[RED]Please Visit [/]`{dataPackage.WorldName}` [RED]Before Installing This Pack[/]!");
+                    Thread.Sleep(2000);
                 }
                 else
                 {
-                    throw new Exception("__data entry not found in the ZIP archive.");
-                }
-            }
-        }
+                    DirectoryInfo lastEditedDirectory = subDirectories
+                        .OrderByDescending(d => d.LastWriteTime)
+                        .FirstOrDefault();
 
-        public DataPackage GetDataPackage()
-        {
-            using (var memoryStream = new MemoryStream(_packData))
-            using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Read))
-            {
-                var packageEntry = archive.GetEntry("package.json");
-                if (packageEntry != null)
-                {
-                    using (var packageStream = packageEntry.Open())
-                    using (var reader = new StreamReader(packageStream))
+                    if (lastEditedDirectory == null)
                     {
-                        var packageJson = reader.ReadToEnd();
-                        return JsonSerializer.Deserialize<DataPackage>(packageJson);
+                        AnsiConsole.MarkupLine($"[RED]Please Visit [/]`{dataPackage.WorldName}` [RED]Before Installing This Pack[/]!");
+                        Thread.Sleep(2000);
+                    }
+                    else
+                    {
+
+                        var data = Pack.GetDataBytes();
+                        var targetFilePath = Path.Combine(lastEditedDirectory.FullName, "__data");
+                        File.WriteAllBytes(targetFilePath, data);
+
+                        File.WriteAllText(Path.Combine(lastEditedDirectory.FullName, "WorldData.csv"),
+                            $"name,version\n{dataPackage.WorldName.Replace(" ", "").ToLower()},{dataPackage.Version.Trim()}");
+
+                        AnsiConsole.MarkupLine($"[GREEN]Pack installed successfully![/]");
+                        Thread.Sleep(2000);
                     }
                 }
-                else
-                {
-                    throw new Exception("package.json entry not found in the ZIP archive.");
-                }
             }
         }
     }
 
-    public class DataPackage
+    public static string GetVRChatPath() =>
+        Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),"..\\LocalLow\\VRChat\\VRChat\\");
+    public static VRCConfig GetVRChatConfig()
     {
-        public string WorldName { get; set; } = "";
-        public string WorldHash { get; set; } = "";
-        public string Version { get; set; } = "";
-        public string Author { get; set; } = "";
-        public string Discord { get; set; } = "";
-        public string Email { get; set; } = "";
+        var ConfigData = File.ReadAllText(GetVRChatPath() + "config.json");
+        return JsonSerializer.Deserialize<VRCConfig>(ConfigData);
+    }
+    public static string GetVRChatContentCachePath()
+    {
+        VRCConfig Conf = GetVRChatConfig();
+        if (string.IsNullOrWhiteSpace(Conf.cache_directory))
+            throw new ArgumentNullException();
+        return Path.Combine(Conf.cache_directory, "Cache-WindowsPlayer");
+    }
+}
+public class VRCConfig
+{
+    public int cache_expire_delay
+    {
+        get; set;
+    } = 0;
+    public int cache_size
+    {
+        get; set;
+    } = 0;
+    public string cache_directory
+    {
+        get; set;
+    } = "";
+    public int fpv_steadycam_fov
+    {
+        get; set;
+    } = 0;
+    public int camera_res_height
+    {
+        get; set;
+    } = 0;
+    public int camera_res_width
+    {
+        get; set;
+    } = 0;
+}
+
+public class DataPack
+{
+    private readonly byte[] _packData;
+
+    public DataPack(byte[] packData)
+    {
+        _packData = packData;
     }
 
+    public static bool ValidPack(byte[] Pack)
+    {
+        try
+        {
+            using var memoryStream = new MemoryStream(Pack);
+            using var archive = new ZipArchive(memoryStream, ZipArchiveMode.Read);
+            var hasPackageJson = false;
+            var hasData = false;
+
+            foreach (var entry in archive.Entries)
+            {
+                if (entry.FullName == "package.json")
+                    hasPackageJson = true;
+                if (entry.FullName == "__data")
+                    hasData = true;
+            }
+
+            return hasPackageJson && hasData;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public byte[] GetDataBytes()
+    {
+        using var memoryStream = new MemoryStream(_packData);
+        using var archive = new ZipArchive(memoryStream, ZipArchiveMode.Read);
+        var dataEntry = archive.GetEntry("__data");
+        if (dataEntry != null)
+        {
+            using var dataStream = dataEntry.Open();
+            using var dataMemoryStream = new MemoryStream();
+            dataStream.CopyTo(dataMemoryStream);
+            return dataMemoryStream.ToArray();
+        }
+        else
+            throw new Exception("__data entry not found in the ZIP archive.");
+    }
+
+    public DataPackage GetDataPackage()
+    {
+        using var memoryStream = new MemoryStream(_packData);
+        using var archive = new ZipArchive(memoryStream, ZipArchiveMode.Read);
+        var packageEntry = archive.GetEntry("package.json");
+        if (packageEntry != null)
+        {
+            using var packageStream = packageEntry.Open();
+            using var reader = new StreamReader(packageStream);
+            var packageJson = reader.ReadToEnd();
+            return JsonSerializer.Deserialize<DataPackage>(packageJson, DataPackageOptions);
+        }
+        else
+            throw new Exception("package.json entry not found in the ZIP archive.");
+    }
+    static readonly JsonSerializerOptions DataPackageOptions = new()
+    {
+        TypeInfoResolver = DataPackageJsonContext.Default
+    };
+}
+
+[JsonSerializable(typeof(DataPackage))]
+public partial class DataPackageJsonContext : JsonSerializerContext
+{
+}
+public class DataPackage
+{
+    public string WorldName { get; set; } = "";
+    public string WorldHash { get; set; } = "";
+    public string Version { get; set; } = "";
+    public string Author { get; set; } = "";
+    public string Discord { get; set; } = "";
+    public string Email { get; set; } = "";
 }
